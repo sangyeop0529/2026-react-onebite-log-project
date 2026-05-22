c178b81b76f1f1ad60bbfdbfc849a29c8cff4b13
sangyeop0529 <sangyeop0529@gmail.com>
2026-05-22 15:17:18 +0900

    sign up 구현하기

---
+++ src/api/auth.ts
@@ -0,0 +1,21 @@
+import { supabase } from "@/lib/supabase";
+
+export async function signUp({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  /**
+   * 구조분해 할당
+   * const response = await supabase.auth.signUp({ email, password });
+   * const data = response.data;
+   * const error = response.error;
+   */
+
+  const { data, error } = await supabase.auth.signUp({ email, password });
+
+  if (error) throw error;
+  return data;
+}
